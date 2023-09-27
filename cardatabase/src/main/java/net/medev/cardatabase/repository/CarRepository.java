9f57beb096972492d3471d29c15ab856b5812d0a
Med Faji <meedfaji@gmail.com>
2023-09-27 16:04:34 +0100

    Use different path naming for the cars services

--- cardatabase/src/main/java/net/medev/cardatabase/repository/CarRepository.java
+++ cardatabase/src/main/java/net/medev/cardatabase/repository/CarRepository.java
@@ -2,6 +2,8 @@ package net.medev.cardatabase.repository;
 
 import net.medev.cardatabase.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="vehicles")
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
