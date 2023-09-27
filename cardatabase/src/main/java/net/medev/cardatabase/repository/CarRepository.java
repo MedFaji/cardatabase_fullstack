ec7db00276ea75c43195be3690cd7104354ec399
Med Faji <meedfaji@gmail.com>
2023-09-27 16:20:21 +0100

    Adding some filtering options

--- cardatabase/src/main/java/net/medev/cardatabase/repository/CarRepository.java
+++ cardatabase/src/main/java/net/medev/cardatabase/repository/CarRepository.java
@@ -2,8 +2,15 @@ package net.medev.cardatabase.repository;
 
 import net.medev.cardatabase.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @RepositoryRestResource(path="vehicles")
 public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    List<Car> findByColor(@Param("color") String color);
 }
