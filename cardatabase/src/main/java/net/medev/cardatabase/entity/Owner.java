31d1aa42436d7ac77b2d63cf89b8533a83501827
Med Faji <meedfaji@gmail.com>
2023-09-27 15:41:39 +0100

    Adding Relation between Owner and Car Entities

--- cardatabase/src/main/java/net/medev/cardatabase/entity/Owner.java
+++ cardatabase/src/main/java/net/medev/cardatabase/entity/Owner.java
@@ -1,25 +1,30 @@
 package net.medev.cardatabase.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @NoArgsConstructor
-@AllArgsConstructor
+
 @Setter
 @Getter
 @Entity
 public class Owner {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ownerid;
     private String firstname, lastname;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") // mappedBy means that in the cars class we will find A property which is "owner", which means that an car has an owner
+    private List<Car> cars;
 
+    public Owner(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 }
