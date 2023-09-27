35bc7b38cd9ddb1385289d5b4e5d4491da608246
Med Faji <meedfaji@gmail.com>
2023-09-27 15:41:14 +0100

    Adding testing data

--- cardatabase/src/main/java/net/medev/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/net/medev/cardatabase/CardatabaseApplication.java
@@ -1,16 +1,45 @@
 package net.medev.cardatabase;
 
+import net.medev.cardatabase.entity.Car;
+import net.medev.cardatabase.entity.Owner;
+import net.medev.cardatabase.repository.CarRepository;
+import net.medev.cardatabase.repository.OwnerRepository;
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 
 @SpringBootApplication
-public class CardatabaseApplication {
+public class CardatabaseApplication implements CommandLineRunner {
 
-	private Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+	@Autowired
+	private CarRepository carRepository;
+
+	@Autowired
+	private OwnerRepository ownerRepository;
+
+	private final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Add owner objects and save these to db
+		Owner owner1 = new Owner("John" , "Johnson");
+		Owner owner2 = new Owner("Mary" , "Robinson");
+		ownerRepository.saveAll(Arrays.asList(owner1, owner2));
+
+		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+		carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
+		carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner2));
+		// Fetch all cars and log to console
+		for (Car car : carRepository.findAll()) {
+			logger.info(car.getBrand() + ' ' + car.getModel());
+		}
+	}
 }
