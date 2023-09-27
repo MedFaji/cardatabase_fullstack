eff2159d369c75e1246bc13fae1ed192a48dd7a1
Med Faji <meedfaji@gmail.com>
2023-09-27 13:31:57 +0100

    Car entity added

---
+++ cardatabase/src/main/java/net/medev/cardatabase/entity/Car.java
@@ -0,0 +1,25 @@
+package net.medev.cardatabase.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand, model, color, registedNumber;
+    private int modelYear, price;
+}
