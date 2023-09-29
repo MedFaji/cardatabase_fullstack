9e9258c1a5db0c5f7f3d8b22aae2c3f0475bda9f
Med Faji <meedfaji@gmail.com>
2023-09-29 15:28:19 +0100

    Adding new users to database

--- cardatabase/src/main/java/net/medev/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/net/medev/cardatabase/CardatabaseApplication.java
@@ -2,8 +2,10 @@ package net.medev.cardatabase;
 
 import net.medev.cardatabase.entity.Car;
 import net.medev.cardatabase.entity.Owner;
+import net.medev.cardatabase.entity.User;
 import net.medev.cardatabase.repository.CarRepository;
 import net.medev.cardatabase.repository.OwnerRepository;
+import net.medev.cardatabase.repository.UserRepository;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -21,6 +23,9 @@ public class CardatabaseApplication implements CommandLineRunner {
 
 	@Autowired
 	private OwnerRepository ownerRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	private final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 	public static void main(String[] args) {
@@ -41,5 +46,11 @@ public class CardatabaseApplication implements CommandLineRunner {
 		for (Car car : carRepository.findAll()) {
 			logger.info(car.getBrand() + ' ' + car.getModel());
 		}
+
+		// Username: user, password: user
+		userRepository.save(new User("user", "$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue","USER"));
+
+		// Username: admin, password: admin
+		userRepository.save(new User("admin", "$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
 	}
-}
+	}
