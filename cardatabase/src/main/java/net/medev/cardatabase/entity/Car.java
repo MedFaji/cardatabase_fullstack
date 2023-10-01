986ddfa98f2f19bc2afcc1993d8cdff1ed010528
Med Faji <meedfaji@gmail.com>
2023-10-01 21:33:02 +0100

    Change name of a filed in Car entity

--- cardatabase/src/main/java/net/medev/cardatabase/entity/Car.java
+++ cardatabase/src/main/java/net/medev/cardatabase/entity/Car.java
@@ -17,18 +17,18 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String brand, model, color, registedNumber;
+    private String brand, model, color, registerNumber;
     private int modelYear, price;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ownerid")
     private Owner owner;
 
-    public Car(String brand, String model, String color, String registedNumber, int modelYear, int price, Owner owner) {
+    public Car(String brand, String model, String color, String registerNumber, int modelYear, int price, Owner owner) {
         this.brand = brand;
         this.model = model;
         this.color = color;
-        this.registedNumber = registedNumber;
+        this.registerNumber = registerNumber;
         this.modelYear = modelYear;
         this.price = price;
         this.owner = owner;
